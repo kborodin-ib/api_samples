fb8b844a272c013db278bf318f46f1c69a4e03ee
kborodin-ib <kborodin@interactivebrokers.com>
2025-08-05 15:56:11 +0300

    Update webApiOrders.py

--- webApiOrders.py
+++ webApiOrders.py
@@ -9,6 +9,9 @@ import sys
 import time
 from datetime import datetime
 
+# TODO: Track status changes of single order with quantity > 9000
+# Use http endpoint polling and websockets subscription
+
 BASE_URL = "https://127.0.0.1:5000/v1/api"
 ORDER_URL = BASE_URL + "/iserver/account/DU6036902/orders"
 WS_URL = "wss://127.0.0.1:5000/v1/api/ws"
@@ -25,113 +28,98 @@ logging.basicConfig(level=logging.DEBUG,
                     ])
 logger.info('Started')
 
-orders_to_place = [
-            {
-                "conidex": "14094@IBIS",
-                "orderType": "LMT",
-                "side": "BUY",
-                "price": 60,
-                "tif": "DAY",
-                "outsideRTH": True,
-                "quantity": 100
-            },
-            {
-                "conidex": "14094@IBIS",
-                "orderType": "LMT",
-                "side": "BUY",
-                "price": 60,
-                "tif": "DAY",
-                "outsideRTH": True,
-                "quantity": 100
-            },
-            {
-                "conidex": "14094@IBIS",
-                "orderType": "LMT",
-                "side": "BUY",
-                "price": 60,
-                "tif": "DAY",
-                "outsideRTH": True,
-                "quantity": 100
-            },
-            {
-                "conidex": "14094@IBIS",
-                "orderType": "LMT",
-                "side": "BUY",
-                "price": 60,
-                "tif": "DAY",
-                "outsideRTH": True,
-                "quantity": 100
-            },
-            {
-                "conidex": "14094@IBIS",
-                "orderType": "LMT",
-                "side": "BUY",
-                "price": 60,
-                "tif": "DAY",
-                "outsideRTH": True,
-                "quantity": 100
-            },
-            {
-                "conidex": "14094@IBIS",
-                "orderType": "LMT",
-                "side": "BUY",
-                "price": 60,
-                "tif": "DAY",
-                "outsideRTH": True,
-                "quantity": 100
-            },
-            {
-                "conidex": "14094@IBIS",
-                "orderType": "LMT",
-                "side": "BUY",
-                "price": 60,
-                "tif": "DAY",
-                "outsideRTH": True,
-                "quantity": 100
-            },
-            {
-                "conidex": "14094@IBIS",
-                "orderType": "LMT",
-                "side": "BUY",
-                "price": 60,
-                "tif": "DAY",
-                "outsideRTH": True,
-                "quantity": 100
-            },
-            {
-                "conidex": "14094@IBIS",
-                "orderType": "LMT",
-                "side": "BUY",
-                "price": 60,
-                "tif": "DAY",
-                "outsideRTH": True,
-                "quantity": 100
-            },
-            {
-                "conidex": "14094@IBIS",
-                "orderType": "LMT",
-                "side": "BUY",
-                "price": 60,
-                "tif": "DAY",
-                "outsideRTH": True,
-                "quantity": 100
-            },
+partial_fill_test_orders = [
+        {
+            "conidex": "756733",
+            "orderType": "LMT",
+            "side": "BUY",
+            "price": 60,
+            "tif": "DAY",
+            "quantity": 1000
+        },
+        {
+            "conidex": "320227571",
+            "orderType": "LMT",
+            "side": "BUY",
+            "price": 60,
+            "tif": "DAY",
+            "quantity": 1000
+        },
+        {
+            "conidex": "9579970",
+            "orderType": "LMT",
+            "side": "BUY",
+            "price": 60,
+            "tif": "DAY",
+            "quantity": 1000
+        }
+    ]
 
+large_cap_test_stocks = [
 
+        {
+            "conidex": "265598",
+            "orderType": "LMT",
+            "side": "BUY",
+            "price": 165,
+            "tif": "DAY",
+            "quantity": 1000
+        },
+        {
+            "conidex": "272093",
+            "orderType": "LMT",
+            "side": "BUY",
+            "price": 350,
+            "tif": "DAY",
+            "quantity": 1000
+        },
+        {
+            "conidex": "76792991",
+            "orderType": "LMT",
+            "side": "BUY",
+            "price": 165,
+            "tif": "DAY",
+            "quantity": 1000
+        },
+        {
+            "conidex": "14094",
+            "orderType": "LMT",
+            "side": "BUY",
+            "price": 85,
+            "tif": "DAY",
+            "quantity": 1000
+        }
 
-        ]
+]
 
 # Suppress messages
 def suppressMessageIDS():
     suppress_url = BASE_URL + "/iserver/questions/suppress"
     payload = json.dumps({
-        "messageIds": ["o102,o163,o10331,o383,o451,o354"]
+        "messageIds": ["o102,o163,o10331,o383,o451,o354,o0"]
     })
     headers = {'Content-Type': 'application/json'}
     response = requests.post(suppress_url, headers=headers, data=payload, verify=False)
     print("Suppress: ", response.text)
 
-# Place one or more orders
+def confirmOrder(replyId):
+    data = {'confirmed': True}
+    message = {}
+    print("REPLY ID: ", replyId)
+    endpoint = BASE_URL + "/iserver/reply/replyId".replace('replyId', replyId)
+    while 'order_id' not in message.keys():
+        print("Incoming: ", message)
+        response = requests.post(endpoint, verify=False, json=data)
+        try:
+            jsonData = json.loads(response.text)
+            print("Outcoming reply endpoint response: ", jsonData)
+            if type(jsonData) == list and 'id' in jsonData[0].keys():
+                print("Multiple orders payload requires confiramtions")
+                rid = jsonData[0]['id']
+                endpoint = endpoint.replace(replyId, rid)
+            return message
+        except Exception as err:
+            print(f"[+] Order was not confirmed")
 
 def placeOrdersIndividually(order_list, max_retries=3, retry_delay=2, side="BUY"):
     """
@@ -161,7 +149,8 @@ def placeOrdersIndividually(order_list, max_retries=3, retry_delay=2, side="BUY"
                     print(f"Response: {response.text}")
                     success = True
                     order_data = json.loads(response.text)[0]
-                    print(order_data)
+                    if 'id' in order_data.keys():
+                        order_data = confirmOrder(order_data['id'])
                     if order_data and int(order_data['order_id']) % 3 == 0:
                         print(f"Canceling order with id {order_data['order_id']}")
                         cancelOrder(order_data)
@@ -199,6 +188,24 @@ def cancelOrder(order_info):
 
     else:
         print(f"❌ Failed to cancel order {order_info['order_id']}: {response.status_code} - {response.text}")
+
+def compareLiveOrders():
+    return
+
+def pollOrderStatus(orderId):
+
+    url = BASE_URL + f"/iserver/account/order/status/{orderId}"
+    known_statuses = ['Submitted']
+
+    while True:
+
+        response = requests.get(url, verify=False)
+        jsonData = json.loads(response.text)
+        if jsonData['order_status'] not in known_statuses:
+            known_statuses.append(jsonData['order_status'])
+            print(known_statuses)
+            with open("statusUpdates.json", "a") as outFile:
+                json.dump(response.text, outFile, indent=4)
 
 
 # Monitor orders via WebSocket
@@ -243,7 +250,8 @@ def startPlacingOrders(order_list):
 
 def infiniteLoop():
     while True:
-        placeOrdersIndividually(orders_to_place, 3, 2)
+        placeOrdersIndividually(large_cap_test_stocks, 3, 2)
+        time.sleep(60)
 
 # Main execution
 if __name__ == "__main__":
@@ -259,16 +267,16 @@ if __name__ == "__main__":
 
 
         # Start monitoring and placing orders in parallel
-        monitor_thread = threading.Thread(target=startMonitoring, args=(messages,))
+        monitor_thread = threading.Thread(target=pollOrderStatus, args=(1360969063,))
       #  order_thread = threading.Thread(target=placeOrdersIndividually, args=(orders_to_place, 3, 2))
-        infiniteThread = threading.Thread(target=infiniteLoop, args=())
+       # infiniteThread = threading.Thread(target=infiniteLoop, args=())
 
-      #  monitor_thread.start()
+        monitor_thread.start()
       #  time.sleep(5)
-        infiniteThread.start()
+      #  infiniteThread.start()
 
-      #  monitor_thread.join()
-        infiniteThread.join()
+        monitor_thread.join()
+       # infiniteThread.join()
     except KeyboardInterrupt:
         # Cancels order updates
         asyncio.run(monitorOrders("uor+{}"))
