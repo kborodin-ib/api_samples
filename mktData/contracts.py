9053b2c5e6ee3b53a7c0456aa1dee79e809f770a
kborodin-ib <kborodin@interactivebrokers.com>
2025-09-11 09:44:37 +0300

    Update contracts.py

--- mktData/contracts.py
+++ mktData/contracts.py
@@ -8,6 +8,412 @@ class CustomContracts():
     def __init__(self):
         self.args = ""
 
+    def nqFutures(self):
+
+        contract = Contract()
+
+        contract.symbol = "NQ"
+        contract.secType = "FOP"
+        contract.exchange = "CME"
+        contract.multiplier = 20
+        contract.right = "C"
+        contract.conId = 799532732
+
+        return contract
+
+    def oilyContract(self):
+
+        contract = Contract()
+        contract.exchange = "CBOT"
+        contract.symbol = "ZL"
+        contract.secType = "FUT"
+        contract.conid = 671574028
+        contract.lastTradeDateOrContractMonth = "20260114"
+        contract.multiplier = 60000
+
+        return contract
+
+    def esFuturesContractSept(self):
+
+        contract = Contract()
+
+        contract.conId = "711280067"
+        contract.symbol = "NQ"
+        contract.symbol = "MES"
+        contract.exchange = "CME"
+
+        return contract
+
+    def futuresContract(self):
+
+        contract = Contract()
+
+        contract.symbol = "MES"
+        contract.exchange = "CME"
+        contract.secType = "FUT"
+        contract.currency = "USD"
+        contract.lastTradeDateOrContractMonth = "20250919"
+
+        return contract
+
+    def mymContract(self):
+
+        contract = Contract()
+
+        contract.symbol = "MYM"
+        contract.exchange = "CBOT"
+        contract.secType = "FUT"
+        contract.currency = "USD"
+        contract.lastTradeDateOrContractMonth = "20250919"
+
+        return contract
+
+
+    def unknownContract(self):
+
+        contract = Contract()
+
+        contract.conId = 705924553
+        contract.exchange = "SMART"
+
+        return contract
+
+    def clFuturesOptions(self):
+
+        contract = Contract()
+
+        contract.symbol = "LN1U5 C1750"
+        contract.exchange = "NYMEX"
+        contract.lastTradeDateOrContractMonth = "20250905"
+        contract.strike = 1.75
+        contract.multiplier = 10000
+        contract.right = "C"
+        contract.tradingClass = "LN1"
+        contract.secType = "FOP"
+
+        return contract
+
+    def clFuturesOptions(self):
+
+        contract = Contract()
+
+        contract.symbol = "LO1U5 C6575"
+        contract.exchange = "NYMEX"
+        contract.lastTradeDateOrContractMonth = "20250905"
+        contract.strike = 65.75
+        contract.multiplier = 1000
+        contract.right = "P"
+        contract.tradingClass = "LO1"
+        contract.secType = "FOP"
+
+        return contract
+
+    def qcomSMART(self):
+
+        contract = Contract()
+
+        contract.symbol = "QCOM"
+        contract.secType = "STK"
+        contract.exchange = "SMART"
+        contract.currency = "USD"
+
+        return contract
+
+    def nq5mini(self):
+
+        contract = Contract()
+
+        contract.conid = 672387474
+        contract.exchange = "CME"
+        contract.symbol = "NQ"
+        contract.secType = "FUT"
+        contract.lastTradeDateOrContractMonth = "20250620"
+
+        return contract
+
+    def esFuturesContractSept(self):
+
+        contract = Contract()
+
+        contract.conId = "711280067"
+        contract.symbol = "MES"
+        contract.exchange = "CME"
+
+        return contract
+
+    def qcomContract(self):
+
+        contract = Contract()
+
+        contract.symbol = "QCOM"
+        contract.exchange = "ISLAND"
+        contract.currency = "USD"
+        contract.secType = "STK"
+
+        return contract
+
+    def esu5Futures(self):
+
+        contract = Contract()
+        contract.secType = "FUT"
+        contract.lastTradeDateOrContractMonth = "20250919"
+        contract.currency = "USD"
+        contract.multiplier = 50
+        contract.exchange = "CME"
+        contract.tradingClass = "ES"
+        contract.symbol = "ESU5"
+
+        return contract
+
+    def esOptions(self):
+
+        contract = Contract()
+
+        contract.secType = "OPT"
+        contract.lastTradeDateOrContractMonth = "20250718"
+        contract.strike = 45
+        contract.right = "P"
+        contract.currency = "USD"
+        contract.multiplier = 100
+        contract.exchange = "SMART"
+        contract.symbol = "ES  250718P00045000"
+        contract.tradingClass = "ES"
+
+        return contract
+
+    def tqqOption(self):
+
+        contract = Contract()
+
+        contract.symbol = "TQQQ"
+        contract.secType = "OPT"
+        contract.exchange = "SMART"
+        contract.currency = "USD"
+        contract.lastTradeDateOrContractMonth = "20250718"
+        contract.strike = 58
+        contract.right = "P"
+        contract.multiplier = "100"
+
+        return contract
+
+    def esContFut(self):
+
+        contract = Contract()
+
+        contract.secType = "FUT"
+        contract.lastTradeDateOrContractMonth = "20250919"
+        contract.currency = "USD"
+        contract.multiplier = 50
+        contract.exchange = "CME"
+        contract.tradingClass = "ES"
+        contract.symbol = "ESU5"
+
+        return contract
+
+    def daxFutures(self):
+
+        contract = Contract()
+
+        contract.symbol = "EUR"
+        contract.secType = "FUT"
+        contract.lastTradeDateOrContractMonth = "20250920"
+        contract.currency = "EUR"
+        contract.multiplier = 1
+        contract.exchange = "EUREX"
+        contract.tradingClass = "FDXS"
+
+        return contract
+    
+    def tslaContract(self):
+
+        contract = Contract()
+
+        contract.conId = '76792991'
+        contract.secType = "STK"
+        contract.symbol = "TSLA"
+        contract.currency = "USD"
+        contract.exchange = "SMART"
+
+        return contract
+
+    def nyseFANGcontract(self):
+
+        contract = Contract()
+
+        contract.secType = "FUT"
+        contract.lastTradeDateOrContractMonth = "20251219"
+        contract.currency = "USD"
+        contract.multiplier = 5
+        contract.exchange = "NYBOT"
+        contract.tradingClass = "FNG"
+        contract.symbol = "FNGM5"
+
+        return contract
+
+    def expiredOptionsContract(self):
+
+        contract = Contract()
+
+        contract.symbol = "GOOG"
+        contract.secType = "OPT"
+        contract.exchange = "BOX"
+        contract.currency = "USD"
+        contract.lastTradeDateOrContractMonth = "20190315"
+        contract.strike = 1180
+        contract.right = "C"
+        contract.multiplier = 100
+
+        return contract
+
+    def dltrContract(self):
+
+        contract = Contract()
+
+        contract.secType = "OPT"
+        contract.lastTradeDateOrContractMonth = "20250606"
+        contract.strike = 94
+        contract.right = 'C'
+        contract.currency = "USD"
+        contract.multiplier = 100
+        contract.exchange = "SMART"
+        contract.tradingClass = "DLTR"
+        contract.symbol = "DLTR  250606C00094000"
+
+        return contract
+
+    def bmwContract(self):
+
+        contract = Contract()
+
+        contract.symbol = "BMW"
+        contract.exchange = "IBIS"
+        contract.currency = "EUR"
+        contract.secType = "STK"
+
+        return contract
+
+    def matifFutures(self):
+
+        contract = Contract()
+
+        contract.exchange = "MATIF"
+        contract.symbol = "EBMU5"
+        contract.currency = "EUR"
+        contract.lastTradeDateOrContractMonth = "20250910"
+        contract.multiplier = 50
+        contract.tradingClass = "EBM"
+        contract.secType = "FUT"
+
+        return contract
+
+    def sbFutures(self):
+
+        contract = Contract()
+
+        contract.conId = 577421489
+        contract.exchange = "NYBOT"
+
+        return contract
+
+    def goldContFut(self):
+
+        contract = Contract()
+
+        contract.symbol = "1610200A0"
+        contract.exchange = "OSE.JPN"
+        contract.conId = 764466855
+
+        return contract
+
+    def bmwContFut(self):
+
+        contract = Contract()
+
+        contract.symbol = "BMW"
+        contract.secType = "CONTFUT"
+        contract.exchange = "EUREX"
+        contract.currency = "EUR"
+        contract.lastTradeDateOrContractMonth = "20250520"
+        contract.tradingClass = "BMWF"
+        contract.multiplier = 100
+        contract.conId = 705063011
+
+        return contract
+
+    def esFuturesContract(self):
+
+        contract = Contract()
+
+        contract.symbol = "ESM5"
+        contract.lastTradeDateOrContractMonth = "20250620"
+        contract.currency = "USD"
+        contract.multiplier = 50
+        contract.exchange = "CME"
+        contract.tradingClass = "ES"
+        contract.secType = "FUTco"
+
+        return contract
+
+    def appContract(self):
+
+        contract = Contract()
+
+        contract.symbol = "APP"
+        contract.secType = "OPT"
+        contract.exchange = "SMART"
+        contract.currency = "USD"
+        contract.lastTradeDateOrContractMonth = "20250815"
+        contract.right = "C"
+        contract.strike = 490
+
+        return contract
+
+    def appContractStok(self):
+
+        contract = Contract()
+
+        contract.symbol = "APP"
+        contract.secType = "STK"
+        contract.currency = "USD"
+        contract.exchange = "SMART"
+
+        return contract
+
+    def spxOptContract(self):
+
+        contract = Contract()
+
+        contract.conId = 780432873
+        contract.symbol = "SPX"
+        contract.exchange = "SMART"
+
+        return contract
+
+    def currencyContract(self):
+
+        contract = Contract()
+
+        contract.symbol = "EUR"
+        contract.currency = "USD"
+        contract.exchange = "IDEALPRO"
+        contract.secType = "CASH"
+
+        return contract
+
+    def testContract(self):
+
+        contract = Contract()
+
+        contract.symbol = "GCM5"
+        contract.conId = 0
+        contract.secType = "FUT"
+        contract.exchange = "COMEX"
+        contract.currency = "USD"
+        contract.tradingClass = "GC"
+        contract.lastTradeDateOrContractMonth = "202506"
+
+        return contract
+
     def isinAaplContract(self):
 
         contract = Contract()
@@ -151,6 +557,18 @@ class CustomContracts():
         contract.symbol = "AAPL"
         contract.secType = "STK"
         contract.currency = "USD"
+
+        return contract
+
+    def toyotaContract(self):
+
+        contract = Contract()
+
+        contract.exchange = "SMART"
+        contract.secType = "STK"
+        contract.currency = "JPY"
+        contract.symbol = "7203"
+     #   contract.primaryExchange = "TSEJ"
 
         return contract
 
