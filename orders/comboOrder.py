800f039b28d8f6be119505cce771a7414b026c96
kborodin-ib <kborodin@interactivebrokers.com>
2025-11-25 11:47:07 +0200

    Update comboOrder.py

--- orders/comboOrder.py
+++ orders/comboOrder.py
@@ -16,40 +16,48 @@ class TestApp(EClient, EWrapper):
 
     def nextValidId(self, orderId: OrderId):
         mycontract = Contract()
-        mycontract.symbol = "ES,NQ"
+        mycontract.symbol = "SPX"
         mycontract.secType = "BAG"
         mycontract.currency = "USD"
         mycontract.exchange = "SMART"
 
         leg1 = ComboLeg()
-        leg1.conId = 551601561# ES March FUT
-        leg1.ratio = 2 
-        leg1.action = "BUY"
-        leg1.exchange = "CME"
+        leg1.conId = 823940009
+        leg1.ratio = 1
+        leg1.action = "SELL"
+        leg1.exchange = "CBOE"
 
         leg2 = ComboLeg()
-        leg2.conId = 620730920 #NQ March FUT
+        leg2.conId = 823940029
         leg2.ratio = 1
-        leg2.action = "SELL"
-        leg2.exchange = "CME"
+        leg2.action = "BUY"
+        leg2.exchange = "CBOE"
 
-        mycontract.comboLegs = []
-        mycontract.comboLegs.append(leg1)
-        mycontract.comboLegs.append(leg2)
-        
+        leg3 = ComboLeg()
+        leg3.conId = 823939252
+        leg3.ratio = 1
+        leg3.action = "BUY"
+        leg3.exchange = "CBOE"
+
+        leg4 = ComboLeg()
+        leg4.conId = 823939259
+        leg4.ratio = 1
+        leg4.action = "SELL"
+        leg4.exchange = "CBOE"
+
+        mycontract.comboLegs = [leg1, leg2, leg3, leg4]
 
         myorder = Order()
         myorder.orderId = orderId
         myorder.action = "BUY"
-        myorder.orderType = "LMT"
+        myorder.orderType = "MKT"
         myorder.totalQuantity = 1
 
-        myorder.lmtPrice = -11430.25
+#        myorder.lmtPrice = 6.20
 
-        myorder.smartComboRoutingParams = []
-        myorder.smartComboRoutingParams.append(TagValue("NonGuaranteed", "1"))
+        # myorder.smartComboRoutingParams = []
+        # myorder.smartComboRoutingParams.append(TagValue("NonGuaranteed", "0"))
 
-        print("ORDER DETAILS: ", myorder)
 
         self.placeOrder(myorder.orderId, mycontract, myorder)
 
@@ -65,7 +73,8 @@ class TestApp(EClient, EWrapper):
             f"orderId:{orderId}",
             f"contract:{contract}",
             f"order:{order}",
-            # f"orderState:{orderState}",
+            f"orderState:{orderState}",
+            f"contract: {contract.comboLegs}"
         )
 
     def orderStatus(
@@ -99,5 +108,5 @@ class TestApp(EClient, EWrapper):
 
 
 app = TestApp()
-app.connect("192.168.43.222", port, 1001)
+app.connect("127.0.0.1", port, 7496)
 app.run()
